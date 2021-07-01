6889dab49f95e773bc71b0399b9bd7dae8dc0fc2
Tuan Phan <56570502+dinhtuanphan@users.noreply.github.com>
2021-07-01 19:51:02 -0400

    Update coding.py

--- coding.py
+++ coding.py
@@ -1,1 +1,7 @@
-        max_subarray = -math.inf
+
+# negative infinity
+max_subarray = -math.inf
+
+# Program to add two matrices
+# using list comprehension
+result = [[X[i][j] + Y[i][j]  for j in range (len(X[0]))] for i in range(len(X))]
